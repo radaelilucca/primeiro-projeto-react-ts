548e568ace9ea6107871355ec89e9e5c5b9de19e
Lucca Radaeli <radaelidev@gmail.com>
2020-08-26 18:23:37 -0300

    feat: get repos from github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,48 +1,66 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useEffect, useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import { Title, Form, Repositories } from './styles';
 
 import logo from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  html_url: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+
+    const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  };
+
   return (
     <>
       <img src={logo} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={e => handleSubmit(e)}>
+        <input
+          value={newRepo}
+          placeholder="Digite o nome do repositório"
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="test">
-          <img src="https://github.com/radaelilucca.png" alt="Lukita" />
+        {repositories.map(repository => (
+          <a href="test" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>radaelilucca/GoStackRepos</strong>
-            <p>Hackintosh Dell G5 5590 / i7-9750H</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img src="https://github.com/radaelilucca.png" alt="Lukita" />
-
-          <div>
-            <strong>radaelilucca/GoStackRepos</strong>
-            <p>Hackintosh Dell G5 5590 / i7-9750H</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img src="https://github.com/radaelilucca.png" alt="Lukita" />
-
-          <div>
-            <strong>radaelilucca/GoStackRepos</strong>
-            <p>Hackintosh Dell G5 5590 / i7-9750H</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
