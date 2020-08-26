cb3df6609b807e0980d8d7fbb008c1a170ac9442
Lucca Radaeli <radaelidev@gmail.com>
2020-08-26 17:34:23 -0300

    create dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+
+import logo from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img src="https://github.com/radaelilucca.png" alt="Lukita" />
+
+          <div>
+            <strong>radaelilucca/GoStackRepos</strong>
+            <p>Hackintosh Dell G5 5590 / i7-9750H</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img src="https://github.com/radaelilucca.png" alt="Lukita" />
+
+          <div>
+            <strong>radaelilucca/GoStackRepos</strong>
+            <p>Hackintosh Dell G5 5590 / i7-9750H</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img src="https://github.com/radaelilucca.png" alt="Lukita" />
+
+          <div>
+            <strong>radaelilucca/GoStackRepos</strong>
+            <p>Hackintosh Dell G5 5590 / i7-9750H</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
