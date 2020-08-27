1cbe419322d27b5c905d5078c96a6f5ae217837b
Lucca Radaeli <radaelidev@gmail.com>
2020-08-26 21:20:55 -0300

    create repo-page layout and style

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import { Header, User, RepoInfo, IssuesContainer } from './styles';
+
+import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -10,7 +13,47 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1> Repository:{params.repository} </h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <User>
+        <img src="https://github.com/radaelilucca.png" alt="adasd" />
+        <div>
+          <strong>nome do repo</strong>
+          <p>descricao do repo</p>
+        </div>
+      </User>
+      <RepoInfo>
+        <li>
+          <strong>1808</strong>
+          <p>Stars</p>
+        </li>
+        <li>
+          <strong>48</strong>
+          <p>Forks</p>
+        </li>
+        <li>
+          <strong>71</strong>
+          <p>Issues Abertas</p>
+        </li>
+      </RepoInfo>
+      <IssuesContainer>
+        <Link to="asdasd" key="22">
+          <div>
+            <strong>nome da issue</strong>
+            <p>autor da issue</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </IssuesContainer>
+    </>
+  );
 };
 
 export default Repository;
---
+++ src/pages/Repository/styles.ts
@@ -0,0 +1,116 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+    color: #737380;
+    font-size: 16px;
+    text-decoration: none;
+  }
+`;
+
+export const User = styled.div`
+  margin-top: 80px;
+
+  display: flex;
+  align-items: center;
+
+  img {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+  }
+
+  div {
+    margin-left: 32px;
+
+    strong {
+      font-size: 36px;
+      line-height: 42px;
+      color: #3d3d4d;
+    }
+
+    p {
+      font-size: 20px;
+      line-height: 23px;
+      color: #737380;
+      margin-top: 12px;
+    }
+  }
+`;
+
+export const RepoInfo = styled.ul`
+  margin-top: 40px;
+  display: flex;
+  list-style: none;
+
+  li {
+    strong {
+      font-size: 36px;
+      line-height: 42px;
+      color: #3d3d4d;
+    }
+
+    p {
+      font-size: 20px;
+      line-height: 23px;
+      color: #6c6c80;
+    }
+
+    & + li {
+      margin-left: 80px;
+    }
+  }
+`;
+
+export const IssuesContainer = styled.div`
+  margin-top: 80px;
+
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 8px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
